4bbb61b7c2fcaed40f8cc48cd2d34ac2dc399db7
Sandro Santilli <strk@keybit.net>
2010-10-06 16:06:39 +0200

    Drop the 'hover' option, we'll always act on hover. Avoid invoking all callbacks (basically copy lots from the SelectFeature into local code)

--- behaviors/dolpp_behavior_highlight_features.js
+++ behaviors/dolpp_behavior_highlight_features.js
@@ -11,7 +11,7 @@ Drupal.dolpp.HighlightFeatures = OpenLayers.Class(OpenLayers.Control.SelectFeatu
   /* Get an index of drupal fids contained in the given feature
    * (possibly clustered)
    */
-  'getDrupalFids': function(feature) {
+  getDrupalFids: function(feature) {
     var fidcount = 0;
     var fids = [];
     if ( ! feature.cluster ) {
@@ -37,42 +37,82 @@ Drupal.dolpp.HighlightFeatures = OpenLayers.Class(OpenLayers.Control.SelectFeatu
     return fids;
   },
 
-  'isSelected': function(feature) {
-    if ( ! feature.cluster ) {
-      return this.selectedFIDS[feature.drupalFID] !== undefined;
+  matchesSample: function(candidate) {
+    if ( ! candidate.cluster ) {
+      return this.selectedFIDS[candidate.drupalFID] !== undefined;
     } else {
-      for(var i = 0; i < feature.cluster.length; i++) {
-        var pf = feature.cluster[i]; // pseudo-feature
+      for(var i = 0; i < candidate.cluster.length; i++) {
+        var pf = candidate.cluster[i]; // pseudo-feature
         if ( this.selectedFIDS[pf.drupalFID] !== undefined ) return true;
       }
       return false;
     }
   },
 
-  'onSelect': function(feature) {
-
-    if ( this.selectedFIDS ) return;
-
-    this.selectedFIDS = this.getDrupalFids(feature);
-    if ( ! this.selectedFIDS ) return;
-
-    var layer = feature.layer;
-    for(var i = 0; i < layer.features.length; i++) {
-      var pf = layer.features[i];
-      if ( pf === feature ) continue; // we know about this already
-      if ( this.isSelected(pf) ) {
-        this.select(pf); // 'highlight' is implemented in base class
+  unhighlightAll: function() {
+    var layers = this.layers || [this.layer];
+    var layer, feature;
+    for(var l=0; l<layers.length; ++l) {
+      layer = layers[l];
+      for(var i=layer.selectedFeatures.length-1; i>=0; --i) {
+        feature = layer.selectedFeatures[i];
+        this.unhighlight(feature);
       }
+      layer.selectedFeatures = [];
     }
   },
 
-  initialize: function(options) {
-    OpenLayers.Control.SelectFeature.prototype.initialize.apply(this, [options]);
-
-    // TODO: change something here so that events don't stop but can
-    //       be handled by next tool
-    //this.handlers.feature
+  highlight: function(feature) {
+    var style = this.selectStyle || this.renderIntent;
+    var layer = feature.layer;
+    layer.drawFeature(feature, style);
+    layer.selectedFeatures.push(feature);
   },
+
+  highlightLike: function(sample) {
+
+    this.unhighlightAll();
+
+    this.selectedFIDS = this.getDrupalFids(sample);
+    if ( ! this.selectedFIDS ) return;
+
+    var layer = sample.layer;
+    for(var i = 0; i < layer.features.length; i++) {
+      var candidate = layer.features[i];
+      if ( this.matchesSample(candidate) ) {
+        this.highlight(candidate);
+      }
+    }
+
+  },
+
+  initialize: function(layers, options) {
+
+      OpenLayers.Control.prototype.initialize.apply(this, [options]);
+
+      if(this.scope === null) {
+          this.scope = this;
+      }
+
+      this.initLayer(layers);
+
+      var callbacks = {
+          over: this.highlightLike,
+          out: this.unhighlightAll
+      }
+
+      this.callbacks = OpenLayers.Util.extend(callbacks, this.callbacks);
+      this.handlers = {
+          feature: new OpenLayers.Handler.Feature(
+              this, this.layer, this.callbacks,
+              {geometryTypes: this.geometryTypes}
+          )
+      };
+  },
+
+
+  // We'll allow selection of multiple features
+  multiple: true,
 
   CLASS_NAME: 'Drupal.dolpp.HighlightFeatures'
 });
@@ -103,7 +143,7 @@ Drupal.behaviors.dolpp_behavior_highlight_features = function(context) {
   //var control = new OpenLayers.Control.SelectFeature(layers,
   var control = new Drupal.dolpp.HighlightFeatures(layers,
     {
-      'hover': !!options.hover 
+      //'hover': !!options.hover 
       //, autoActivate: true
     });
 
